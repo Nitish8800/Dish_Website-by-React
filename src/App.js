d4f31f03bc7af241ca313d583c1f73825daec710
Nitish8800 <nitishfea120784@gmail.com>
2022-04-27 01:51:15 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import Resturant from "./component/Basics/Resturant";
+
+const App = () => {
+  return (
+    <div>
+      <Resturant />
+    </div>
+  );
+};
+
+export default App;
